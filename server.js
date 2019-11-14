822b167e1bd5a5ef67a510a47e79e5a3e87dfe6d
Naseem <naseemkullah@gmail.com>
2019-11-13 19:36:00 -0500

    Add current span context, trace id in logged error

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('./lib/tracer');
 const express = require('express');
+const opentelemetry = require('@opentelemetry/core');
 const { expressLogger, logger } = require('./lib/logger');
 const jobs = require('./routes/jobs');
 
@@ -11,7 +12,8 @@ app.get('/healthz', (req, res) => res.status(200).end());
 app.use('/api/jobs', jobs);
 // eslint-disable-next-line no-unused-vars
 app.use((err, req, res, next) => {
-  logger.error(err);
+  const spanContext = opentelemetry.getTracer().getCurrentSpan().context();
+  logger.error({ err, spanContext }, `${err}, traceId: ${spanContext.traceId}`);
   res.status(err.code).send(err.message);
 });
 
