927ed71cdfa7a23c7baace2c7eb66bb0e573cebd
Naseem <naseem@transit.app>
2020-06-24 22:53:56 -0400

    Remove unneeded config, add blank line

--- src/lib/tracer.ts
+++ src/lib/tracer.ts
@@ -6,17 +6,11 @@ import {SimpleSpanProcessor} from '@opentelemetry/tracing';
 
 const provider = new NodeTracerProvider({
   logLevel: LogLevel.ERROR,
-  plugins: {
-    http: {
-      enabled: true,
-      path: '@opentelemetry/plugin-http',
-      // ignoreIncomingPaths: [/\/healthz/],
-    },
-  },
   sampler: new ProbabilitySampler(0.5),
 });
 
 const serviceName = 'k8s-job-dispatcher';
+
 const exporter = new JaegerExporter({serviceName});
 
 provider.addSpanProcessor(new SimpleSpanProcessor(exporter));
